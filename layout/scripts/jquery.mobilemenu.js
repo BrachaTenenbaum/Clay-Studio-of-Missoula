c18fdbac55e1bc2644f746e2ab1d4d1a9de36bc9
Bracha <br2341@gmail.com>
2017-10-20 08:16:34 -0600

    remove css

--- layout/scripts/jquery.mobilemenu.js
+++ layout/scripts/jquery.mobilemenu.js
@@ -1,12 +1,10 @@
 // mobile menu dropdown
 function changeMobileMenu() {
     $('#mobilemainav .mobile-menu').click(function mobileMenu() {
-      $('.navbar-mobile').css('padding-top', '0');
       $('#mobilemainav .menu').show();
     });
   
     $('#mobilemainav .menu .close').click(function closeMobileMenu() {
-    //   $('.navbar-mobile').css('padding-top', '15px');
       $('#mobilemainav .menu').hide();
     });
   }
